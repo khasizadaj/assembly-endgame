3d8311d504d46ecdaaad385c55bfd74c7f1a2f93
jkhasizada <javid.khasizada@gmail.com>
2025-01-27 23:53:16 +0400

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <main>
+      <section className="flex flex-col justify-center items-center gap-4 h-screen bg-slate-900">
+        <h1 className="text-4xl font-bold text-slate-50">Assembly: Endgame</h1>
+        <p className="text-xl text-slate-50">
+          Guess the word in under 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </section>
+    </main>
+  );
+}
+
+export default App;
