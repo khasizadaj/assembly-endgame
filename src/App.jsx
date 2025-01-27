feaf97ea48c325f6edbe9128bc3c0dfa8b9445e1
jkhasizada <javid.khasizada@gmail.com>
2025-01-28 00:38:05 +0400

    Add initial plan

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-function App() {
+import Header from "./components/Header";
+
+export default function App() {
+  // state values:
+  //   guessed word
+  //   letters to choose from
+  //   last answer (with status, i.e. correct, incorrect)
+  //   languages
+
+  // derived values:
+  //   statusMessage -> from last answer
+  //   gameResult -> from languages (if no language is left, game is over)
+
   return (
-    <main>
-      <section className="flex flex-col justify-center items-center gap-4 h-screen bg-slate-900">
-        <h1 className="text-4xl font-bold text-slate-50">Assembly: Endgame</h1>
-        <p className="text-xl text-slate-50">
-          Guess the word in under 8 attempts to keep the programming world safe
-          from Assembly!
-        </p>
-      </section>
+    <main className="flex flex-col justify-start items-center gap-4 min-h-screen bg-slate-900 p-4 sm:p-12">
+      <Header />
+      {/* Section: Status Update */}
+      {/* Section: Languages */}
+      {/* Section: Guessed Word */}
+      {/* Section: Letters to choose from */}
+      {/* Section: New Game Button */}
     </main>
   );
 }
-
-export default App;
