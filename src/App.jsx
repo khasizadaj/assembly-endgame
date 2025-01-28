004ff69ba67d5b99237f5b3d07afafedb49805d9
jkhasizada <javid.khasizada@gmail.com>
2025-01-28 19:38:18 +0400

    Add status container

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import Header from "./components/Header";
+import Status from "./components/Status";
 
 export default function App() {
+  const statusType = "incorrect"; // TODO derive from last answer
+
   // state values:
   //   guessed word
   //   letters to choose from
   //   last answer (with status, i.e. correct, incorrect)
   //   languages
-
   // derived values:
-  //   statusMessage -> from last answer
   //   gameResult -> from languages (if no language is left, game is over)
-
   return (
-    <main className="flex flex-col justify-start items-center gap-4 min-h-screen bg-slate-900 p-4 sm:p-12">
+    <main className="flex flex-col justify-start items-center gap-12 min-h-screen bg-slate-900 p-4 sm:p-12">
       <Header />
-      {/* Section: Status Update */}
+      <Status statusType={statusType} />
       {/* Section: Languages */}
       {/* Section: Guessed Word */}
       {/* Section: Letters to choose from */}
---
+++ src/components/Status.jsx
@@ -0,0 +1,20 @@
+export default function Status(props) {
+  const messages = {
+    incorrect: ["Farewell belowed programing language.", "Seriously!?"],
+    correct: ["Correct"],
+    gameOver: "Game Over",
+  };
+
+  const statusMessage =
+    messages[props.statusType][
+      Math.floor(Math.random() * messages[props.statusType].length)
+    ];
+
+  return (
+    <section>
+      <div className="max-w-md w-full bg-green-600 py-4 px-12 rounded-lg">
+        <h1 className="text-base sm:text-xl text-center">{statusMessage}</h1>
+      </div>
+    </section>
+  );
+}
