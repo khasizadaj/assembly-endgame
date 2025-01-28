d2d0142f4296647f88d253297e4df966a9d2cb11
jkhasizada <javid.khasizada@gmail.com>
2025-01-28 22:32:15 +0400

    Fix typo in component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 import Header from "./components/Header";
 import Status from "./components/Status";
-import Languages from "./components/Langauges";
+import Languages from "./components/Languages";
 
 export default function App() {
   const statusType = "incorrect"; // TODO derive from last answer
