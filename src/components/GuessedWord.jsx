28de6bd3c5295dd427e9c02e39a7d2fe659d8f4e
jkhasizada <javid.khasizada@gmail.com>
2025-02-02 14:58:13 +0400

    Fix color bug in guessed word

--- src/components/GuessedWord.jsx
+++ src/components/GuessedWord.jsx
@@ -3,12 +3,14 @@ import clsx from "clsx";
 function LetterBox(props) {
   const classes = clsx(
     "py-1 px-2 rounded-lg font-semibold w-8 h-8 text-center",
-    props.isFound ? "bg-green-500" : "text-transparent bg-slate-600",
+    props.isFound ? "bg-green-500" : "bg-slate-600",
     props.statusType === "gameOver" && !props.isFound
-      ? "bg-yellow-400 text-inherit"
+      ? "bg-yellow-400"
       : null
   );
-  return <span className={classes}>{props.value}</span>;
+  return <span className={classes}>
+    {(props.isFound || props.statusType == "gameOver") && props.value}
+    </span>;
 }
 
 export default function GuessedWord(props) {
