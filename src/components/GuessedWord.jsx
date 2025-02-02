178f4fff1a5e967d48769bca9667b34a747155d7
jkhasizada <javid.khasizada@gmail.com>
2025-02-02 14:39:11 +0400

    Some enhancements
    - Add game over logic (status message update, buttons are disabled)
    - Not found letters are shown to user when game ends
    - Status message update after each choice

--- src/components/GuessedWord.jsx
+++ src/components/GuessedWord.jsx
@@ -3,7 +3,10 @@ import clsx from "clsx";
 function LetterBox(props) {
   const classes = clsx(
     "py-1 px-2 rounded-lg font-semibold w-8 h-8 text-center",
-    props.isFound ? "bg-green-500" : "text-transparent bg-slate-600"
+    props.isFound ? "bg-green-500" : "text-transparent bg-slate-600",
+    props.statusType === "gameOver" && !props.isFound
+      ? "bg-yellow-400 text-inherit"
+      : null
   );
   return <span className={classes}>{props.value}</span>;
 }
@@ -12,7 +15,7 @@ export default function GuessedWord(props) {
   return (
     <div className="flex justify-center gap-2 flex-wrap">
       {props.word.map((letter, index) => (
-        <LetterBox key={index} value={letter.value} isFound={letter.isFound} />
+        <LetterBox key={index} value={letter.value} isFound={letter.isFound} statusType={props.statusType} />
       ))}
     </div>
   );
